35e563020a8009bbe6bf22de91673124d4c8da12
Henry <3134949983@qq.com>
2022-02-14 01:17:51 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
       </transition>
     </router-view>
     <Tabbar placeholder route v-show="showFooter">
-      <TabbarItem icon="home-o" replace to="/">Home</TabbarItem>
+      <TabbarItem icon="home-o" replace to="/">Home1</TabbarItem>
       <TabbarItem icon="search" replace to="/about">About</TabbarItem>
       <TabbarItem icon="friends-o" replace to="/todoList">ToDo</TabbarItem>
       <TabbarItem icon="setting-o" replace to="/luckdraw">LuckDraw</TabbarItem>
